f1f19187ebe3f31f0d255c5bd409cdd7443d4a59
Denis Grab <166665464+TheNimbusX@users.noreply.github.com>
2025-06-21 12:11:15 +0300

    update cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -15,7 +15,7 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['https://alice-baturova.ru/'],
+      origin: ['https://alice-baturova.ru'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       credentials: true,
